5e39d9bb45accb7b4384d55a789319b3ff9ceb61
ducmanh <ducmanh@DESKTOP-KBVVIP0>
2023-03-31 10:21:13 +0700

    Add project files.

---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Toi Ten hieu");
+            Console.ReadKey(true);
+        }
+    }
+}
